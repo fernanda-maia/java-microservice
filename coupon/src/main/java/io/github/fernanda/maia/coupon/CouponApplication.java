c584d2ab0eda2b9467d71631d6d6402d963164d1
Fernanda Ribeiro <fer.maiafr@gmail.com>
2021-11-03 13:56:02 -0300

    init: Start spring project

---
+++ coupon/src/main/java/io/github/fernanda/maia/coupon/CouponApplication.java
@@ -0,0 +1,11 @@
+package io.github.fernanda.maia.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CouponApplication.class, args);
+    }
+}
